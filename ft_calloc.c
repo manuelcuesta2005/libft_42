b6d0c4695229ec163a0bbf825d51e2904af3a8a2
macuba2005 <107274670+manuelcuesta2005@users.noreply.github.com>
2024-10-15 11:23:32 +0200

    mi primera libreria :D

--- ft_calloc.c
+++ ft_calloc.c
@@ -21,9 +21,7 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	total_size = nmemb * size;
 	ptr = malloc(total_size);
 	if (ptr == NULL)
-	{
 		return (NULL);
-	}
 	ft_memset(ptr, 0, total_size);
 	return (ptr);
 }
